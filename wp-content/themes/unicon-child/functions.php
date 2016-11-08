af302b8f34510a8f668247bbba746e761a24753d
Paweł Mikicin <mikuspawel2@gmail.com>
2016-11-08 13:42:05 +0100

    Start project

---
+++ wp-content/themes/unicon-child/functions.php
@@ -0,0 +1,11 @@
+<?php
+ 
+/* zmiana obrazka logo na ekranie logowania 
+ 
+function custom_login_logo() {
+echo '<style type="text/css">
+h1 a { background-image: url('.get_bloginfo('template_directory').'/images/admin-logo.jpg) !important; }
+</style>';
+}
+ 
+add_action('login_head','custom_login_logo'); */
